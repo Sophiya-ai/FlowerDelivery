284b67506c944dc818b644266f9a96d3bbac0a6d
Sophiya-ai <sophiyabr@gmail.com>
2025-02-19 08:26:55 +0200

    правка приветствия и проверки в боте

--- bot/main.py
+++ bot/main.py
@@ -34,12 +34,20 @@ async def start(message: Message):
     args = message.text.split()[1:]
     user_id_txt = args[0] if args else None # Берем первый аргумент, если он есть
     user_id = int(user_id_txt)
-    await bot.send_message(message.chat.id, f"Привет! Вы успешно подключили свой аккаунт (ID: {user_id}).")
+
     conn = create_db_connection()  # соединение с БД
 
     if conn is not None:
         cursor = conn.cursor()
         try:
+            # Проверяем, существует ли пользователь с таким ID среди пользователей
+            cursor.execute("SELECT id FROM shop_UserProfile WHERE id = ?", (user_id,))
+            user_exists = cursor.fetchone()
+
+            if not user_exists:
+                await message.reply("Пользователь с таким ID не найден.")
+                return
+
             if user_id:
                 # Проверяем, есть ли уже пользователь с таким telegram_id
                 cursor.execute("SELECT telegram_id FROM shop_BotUser WHERE telegram_id = ?", (message.from_user.id,))
@@ -50,29 +58,32 @@ async def start(message: Message):
                         f"Теперь вы будете получать уведомления о ваших заказах.")
                     return
 
-                # Если ID пользователя есть, сохраняем данные в таблицу BotUser
+                # Если ID пользователя с сайта получено, сохраняем данные в таблицу BotUser
                 cursor.execute("""
                     INSERT INTO shop_BotUser (telegram_id, username, first_name, last_name)
                     VALUES (?, ?, ?, ?)
                 """, (message.from_user.id, message.from_user.username, message.from_user.first_name,
                       message.from_user.last_name))
 
+                # Обновляем UserProfile
                 cursor.execute(""" UPDATE shop_UserProfile SET telegram_user_id = ? WHERE id = ?
                                     """, (message.from_user.id, user_id,))
                 conn.commit()
 
-                # Логируем добавление пользователя
-                logging.info(
-                    f"Добавлен новый пользователь в BotUser с telegram_id: {message.from_user.id}")
+                # Получаем имя пользователя для приветствия
+                cursor.execute("SELECT first_name FROM shop_UserProfile WHERE id = ?", (user_id,))
+                user_data = cursor.fetchone()
+                user_first_name = user_data[0] if user_data else "Пользователь"
 
+                # Логируем создание ботюзера и связку IDs
+                logging.info(f"Пользователь с ID {user_id} успешно связан с Telegram ID {message.from_user.id}")
                 await message.reply(
-                    f"Привет! Вы успешно подключены к вашему аккаунту на сайте. "
-                    f"Теперь вы будете получать уведомления о ваших заказах.")
+                    f"Привет, {user_first_name}! Вы успешно подключили свой аккаунт. Теперь вы будете получать уведомления.")
 
-            else:
+        else:
                 # ID пользователя отсутствует, пользователь запустил бота без подключения к сайту
                 await message.reply(
-                    "Привет! Чтобы получать информацию о ваших заказах, подключитесь к боту на странице истории заказов на нашем сайте.")
+                    "Привет! Чтобы получать информацию о ваших заказах, подключитесь к боту на нашем сайте.")
 
         except sqlite3.Error as e:
             logging.error(f"Ошибка при работе с БД: {e}")  # Логируем ошибки БД
